d04ed2a205123feececc3d5217316edee7194944
Fireflash <sam@trnl.co>
2023-09-26 12:29:31 +1000

    update coin to align with ERD

--- GroupWork08/Assets/Scripts/Coin.cs
+++ GroupWork08/Assets/Scripts/Coin.cs
@@ -5,16 +5,9 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     [SerializeField] private float rotationSpeed = 180;
-    [SerializeField] private float score = 1;
     
     private void Update()
     {
         transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed, Space.World);
-    }
-
-    public float Collect()
-    {
-        Destroy(gameObject);
-        return score;
     }
 }
